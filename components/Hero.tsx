6181b33bb07eb8dc17cfe64bff311f628b39e191
anthony95 <95attakayny@gmail.com>
2024-05-29 10:16:15 +0000

    navbar & hero

---
+++ components/Hero.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { cn } from "@/lib/utils";
+const Hero = () => {
+  return ( 
+  
+    <section className=" py-16 flex px-8 gap-12 flex-wrap bg-gray-100 ">
+      <div className="pl-8 sm:pl-12 md:pl-16 lg:pl-24 xl:w-2/5  my-auto" >
+      <h1 className="text-4xl pb-4 tracking-wide text-primary-dark font-semibold uppercase">Expert Accounting and Advisory Services</h1>
+      <h2 className="pb-8 w-[90%]  text-secondary-dark leading-relaxed">KAP Chartered Accountants provides comprehensive and innovative professional services to promote sustainable and meaningful growth to our clients' business</h2>
+      <Button className={cn('text-white text-xl hover:text-secondary-dark rounded-none')} >Learn More</Button>
+      <Button className={cn('text-secondary- text-xl')}  variant={"ghost"}>Contact Us</Button>
+      </div>
+    
+    <div className="xl:3/5 xl:flex-1 pl-8 sm:pl-12 md:pl-16 lg:pl-24 xl:pl-0 ">
+
+      <Image className="rounded-r-full"  src={'/hero-image.jpg '} width={1000} height={1500}   alt="image"/>
+    </div>
+     
+    </section>
+   
+   );
+}
+ 
+export default Hero;
---
+++ components/navbar.tsx
@@ -0,0 +1,41 @@
+'use client'
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Menu } from "lucide-react";
+const Navbar = () => {
+  const pathname = usePathname()
+  console.log(pathname)
+
+  const navElements = [
+    { name: "Home", href: "/" },
+    { name: "About Us", href: "/about" },
+    { name: "Services", href: "/services" },
+    { name: "Blog", href: "/blog" },
+    { name: "Contact", href: "/contact" },
+  ];
+  return (
+    <header className=" px-8 sm:px-12 md:px-16 lg:px-24 flex shadow-sm py-4 items-center justify-between ">
+      <div className="flex gap-4">
+        <Image src="/logo.png" width={50} height={50} alt="logo" />
+        <div className=" text-sm leading-4 tracking-normal md:tracking-widest text-primary font-bold my-auto">
+          <h1>KAP</h1>
+          <h1 className="text-secondary">CHARTED ACCOUNTANTS</h1>
+        </div>
+      </div>
+      <nav>
+        <Menu className="md:hidden stroke-primary size-8 hover:stroke-secondary cursor-pointer"/>
+        <ul className="hidden  md:flex text-base lg:text-lg gap-8 lg:gap-12 font-medium text-primary  ">
+          {navElements.map((nav) => (
+            <li key={nav.name} className="hover:text-primary-dark">
+              <Link className={pathname=== nav.href? 'text-secondary-dark':''}  href={nav.href}>{nav.name}</Link>{" "}
+            </li>
+          ))}
+        </ul>
+      </nav>
+   
+    </header>
+  );
+};
+
+export default Navbar;
